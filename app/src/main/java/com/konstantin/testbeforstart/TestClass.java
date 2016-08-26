68eeff8d2a3160b3fd6f6316684fa53a3db66457
BaranovKM <irongrol@gmail.com>
2016-08-26 17:56:15 +0400

    добавлен новый класс

---
+++ app/src/main/java/com/konstantin/testbeforstart/TestClass.java
@@ -0,0 +1,8 @@
+package com.konstantin.testbeforstart;
+
+/**
+ * Created by Константин on 26.08.2016.
+ */
+public class TestClass {
+    //добавлен новый класс, который будет закомитчен в новую ветку
+}
